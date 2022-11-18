c69c2135759dd10ed5120ea82f89e40b2d52db37
Astariul <remondnicola@gmail.com>
2022-11-18 20:51:00 +0900

    ✨ Make prob4 work

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -1,3 +1,4 @@
 pub mod prob_1;
 pub mod prob_2;
 pub mod prob_3;
+pub mod prob_4;
--- src/problems/prob_4.rs
+++ src/problems/prob_4.rs
@@ -1,9 +1,10 @@
 /// Compute the largest palindrome made of the product of x digits, where x is
 /// given.
-fn largest_palindrome_product(n_digits: usize) -> usize {
+fn largest_palindrome_product(n_digits: u32) -> usize {
+    let max_digit = 10_usize.pow(n_digits);
     let mut largest = 0;
-    for i in 1..10.pow(n_digits) {
-        for j in 1..10.pow(n_digits) {
+    for i in 1..max_digit {
+        for j in 1..max_digit {
             let prod = i * j;
             if is_palindromic(prod) && prod > largest {
                 largest = prod;
@@ -15,23 +16,23 @@ fn largest_palindrome_product(n_digits: usize) -> usize {
 
 /// Function checking if the given number is palindromic (if it can read both
 /// way).
-///
-/// # Example
-/// ```
-/// assert!(is_palindromic(909))
-/// assert!(!is_palindromic(9099))
-/// assert!(is_palindromic(9009))
-/// ```
 fn is_palindromic(x: usize) -> bool {
     let x_str = x.to_string();
     let len = x_str.len();
     let mid = len / 2;
 
+    // Depending on the size of the string, consider or not the digit in the middle
+    let s1 = &x_str[0..mid];
+    let s2;
     if len % 2 == 0 {
-        return &x_str[0..mid] == &x_str[mid..len]
+        s2 = &x_str[mid..len];
     } else {
-        return &x_str[0..mid] == &x_str[mid + 1..len]
+        s2 = &x_str[mid + 1..len];
     }
+    
+    // Reverse the second string, and compare them
+    let s2 = s2.chars().rev().collect::<String>();
+    s1 == s2
 }
 
 /// Solve the problem #2 and return the solution.
@@ -44,7 +45,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example() {
+    fn test_given_example() {
         assert_eq!(largest_palindrome_product(2), 9009);
+    }
+
+    #[test]
+    fn test_is_palindromic_true_odd() {
+        assert!(is_palindromic(909));
+    }
+
+    #[test]
+    fn test_is_palindromic_true_even() {
+        assert!(is_palindromic(9009));
+    }
+
+    #[test]
+    fn test_is_palindromic_false_odd() {
+        assert!(!is_palindromic(906));
+    }
+
+    #[test]
+    fn test_is_palindromic_false_even() {
+        assert!(!is_palindromic(9099));
     }
 }
