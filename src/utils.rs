546a3e27572812e8c7bb7ffb28f934e69e5d568e
Astariul <remondnicola@gmail.com>
2022-11-20 14:42:26 +0900

    ✨ Add iterator over prime numbers

--- src/utils.rs
+++ src/utils.rs
@@ -17,3 +17,53 @@ impl Iterator for Fibonacci {
         Some(current)
     }
 }
+
+
+pub struct Primes {
+    p: usize,
+    previous_primes: Vec<usize>,
+}
+
+impl Primes {
+    pub fn new() -> Self {
+        Self {
+            p: 1,
+            previous_primes: vec![2],
+        }
+    }
+}
+
+// Implement `Iterator` for `Primes`.
+impl Iterator for Primes {
+    type Item = usize;
+
+    // Define how to compute the next prime number.
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.p < 3 {
+            // Special cases for the first primes : 2 and 3 are primes. After
+            // that we can just skip even numbers.
+            self.p += 1;
+        } else {
+            loop {
+                self.p += 2;    // Skip even numbers
+
+                // Check if the number is divisible by any previous prime
+                let mut divisible = false;
+                for pp in &self.previous_primes {
+                    if self.p % pp == 0 {
+                        divisible = true;
+                        break;
+                    }
+                }
+
+                if !divisible {
+                    // We found the next prime !
+                    break;
+                }
+            }
+        }
+
+        self.previous_primes.push(self.p);
+        Some(self.p)
+    }
+}
