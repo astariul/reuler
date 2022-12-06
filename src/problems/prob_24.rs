110127adcfb900ee3a55521f0637ce172099f228
Astariul <remondnicola@gmail.com>
2022-12-06 19:42:04 +0900

    ⚡️ Improve performances by implementing permutations as iterator

--- src/problems/prob_24.rs
+++ src/problems/prob_24.rs
@@ -1,52 +1,78 @@
-/// Compute all possible permutations for a given vector of elements.
+/// Iterator to compute all possible permutations for a given vector of
+/// elements.
 ///
 /// # Note
-/// This function returns the permutations in lexical order (the given elements
-/// are first sorted).
-fn permutations(elements: &mut Vec<char>) -> Vec<String> {
-    let mut permuts = vec![];
+/// This Iterator returns the permutations in lexical order (sorted).
+struct Permutations {
+    elements: Vec<char>,
+    is_end: bool,
+}
 
-    // We want the lexical order, so sort the given elements
-    elements.sort();
+impl Permutations {
+    /// Constructor, making sure the elements are sorted.
+    pub fn new(elements: Vec<char>) -> Self {
+        let mut elements = elements.clone();
 
-    loop {
-        // Add the current permutation (turn the list of characters into string before)
-        let current_permutation = elements.clone().iter().map(|c| c.to_string()).collect();
-        permuts.push(current_permutation);
+        // We want the lexical order, so sort the given elements
+        elements.sort();
 
-        // get the next permutation in lexicographical order
-        let mut i = elements.len() - 1;
-        while i > 0 && elements[i - 1] >= elements[i] {
+        Self { elements: elements, is_end: false }
+    }
+}
+
+/// Implement `Iterator` for `Permutations`.
+impl Iterator for Permutations {
+    type Item = String;
+
+    /// Define how to compute the next step.
+    fn next(&mut self) -> Option<Self::Item> {
+        // If the last iteration was the last one, is_end is true : return None
+        if self.is_end {
+            return None;
+        }
+
+        // Get the current permutation (turn the list of characters into string before)
+        // This is what we will return for this iteration
+        let current_permutation = self.elements.clone().iter().map(|c| c.to_string()).collect();
+
+        // From here, compute the next permutation in lexicographical order
+        let mut i = self.elements.len() - 1;
+        while i > 0 && self.elements[i - 1] >= self.elements[i] {
             i -= 1;
         }
 
-        // If there are no more permutations, break out of the loop
+        // If there are no more permutations, this is the last iteration
+        // (the next one should return None)
         if i == 0 {
-            break;
+            self.is_end = true;
+        } else {
+            let mut j = self.elements.len() - 1;
+            while self.elements[j] <= self.elements[i - 1] {
+                j -= 1;
+            }
+
+            self.elements.swap(i - 1, j);
+            self.elements[i..].reverse();
         }
 
-        let mut j = elements.len() - 1;
-        while elements[j] <= elements[i - 1] {
-            j -= 1;
-        }
-
-        elements.swap(i - 1, j);
-        elements[i..].reverse();
+        // Return the current permutation !
+        Some(current_permutation)
     }
-
-    permuts
 }
 
 
 /// Compute the n-th permutation for up to the given digit.
 fn n_permutation(digit_limit: usize, n: usize) -> String {
-    let mut digits = (0..digit_limit + 1).map(|d| char::from_digit(d as u32, 10).unwrap()).collect();
-    let mut permuts = permutations(&mut digits);
+    let digits = (0..digit_limit + 1).map(|d| char::from_digit(d as u32, 10).unwrap()).collect();
+    let permuts = Permutations::new(digits);
 
-    let x = permuts.len();
+    for (i, p) in permuts.enumerate() {
+        if i == n - 1 {
+            return p;
+        }
+    }
 
-    // Return the right indice
-    permuts.remove(n - 1)
+    panic!("The given n ({n}) is too big, there is not that much permutations. Provide a smaller n.")
 }
 
 /// Solve the problem #24 and return the solution.
@@ -70,14 +96,13 @@ mod tests {
 
     #[test]
     fn test_permutations() {
-        let r = permutations(&mut (0..3).map(|d| char::from_digit(d as u32, 10).unwrap()).collect());
-        assert_eq!(r.len(), 6);
-        assert!(r.contains(&String::from("012")));
-        assert!(r.contains(&String::from("021")));
-        assert!(r.contains(&String::from("102")));
-        assert!(r.contains(&String::from("120")));
-        assert!(r.contains(&String::from("012")));
-        assert!(r.contains(&String::from("201")));
-        assert!(r.contains(&String::from("210")));
+        let mut permuts = Permutations::new((0..3).map(|d| char::from_digit(d as u32, 10).unwrap()).collect());
+        assert_eq!(permuts.next().unwrap(), String::from("012"));
+        assert_eq!(permuts.next().unwrap(), String::from("021"));
+        assert_eq!(permuts.next().unwrap(), String::from("102"));
+        assert_eq!(permuts.next().unwrap(), String::from("120"));
+        assert_eq!(permuts.next().unwrap(), String::from("201"));
+        assert_eq!(permuts.next().unwrap(), String::from("210"));
+        assert!(permuts.next().is_none());
     }
 }
