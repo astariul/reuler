2ad3c11e289b4985e706824b164b917f6723d8ee
Astariul <remondnicola@gmail.com>
2022-12-07 23:56:22 +0900

    ♻️ Refactor existing problems to use BigInt from utils

--- src/problems/prob_16.rs
+++ src/problems/prob_16.rs
@@ -1,28 +1,16 @@
+use crate::utils;
+
 /// Compute the sum of digit of the number elevated to the given power.
 fn power_digit_sum(x: usize, p: usize) -> usize {
-    // Store each digit in a vector that will grow over time
-    let mut digits = vec![x];
+    // The number will be big, use a BigInt
+    let mut number = utils::BigInt::from(x);
 
     for _ in 1..p {
-        // Multiply each digit by x, and don't forget to carry over !
-        let mut carry_over = 0;
-        for i in 0..digits.len() {
-            let digit_result = digits[i] * x + carry_over;
-
-            // Update current digit
-            digits[i] = digit_result % 10;
-
-            // Update carry over
-            carry_over = digit_result / 10;
-        }
-
-        if carry_over != 0 {
-            // We have a carry over left, add a new digit
-            digits.push(carry_over);
-        }
+        number = number * x;
     }
 
-    digits.iter().sum()
+    // Sum the digits of our big number
+    number.digits.iter().sum()
 }
 
 /// Solve the problem #16 and return the solution.
--- src/problems/prob_20.rs
+++ src/problems/prob_20.rs
@@ -1,31 +1,16 @@
+use crate::utils;
+
 /// Compute the sum of digits of the factorial of the given number.
 fn factorial_digit_sum(x: usize) -> usize {
-    // Store each digit in a vector that will grow over time
-    let mut digits = vec![1];
+    // The number will be big, use a BigInt
+    let mut number = utils::BigInt::from(1);
 
     for mul_factor in 2..x + 1 {
-        // Multiply each digit by x, and don't forget to carry over !
-        let mut carry_over = 0;
-        for i in 0..digits.len() {
-            let digit_result = digits[i] * mul_factor + carry_over;
-
-            // Update current digit
-            digits[i] = digit_result % 10;
-
-            // Update carry over
-            carry_over = digit_result / 10;
-        }
-
-        while carry_over != 0 {
-            // We have a carry over left, add a new digit
-            let new_digit = carry_over % 10;
-            digits.push(new_digit);
-
-            carry_over = carry_over / 10;
-        }
+        number = number * mul_factor;
     }
 
-    digits.iter().sum()
+    // Sum the digits of our big number
+    number.digits.iter().sum()
 }
 
 /// Solve the problem #20 and return the solution.
