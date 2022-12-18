74aa62a6db917400bf9d9432f30275359fba3e6c
Astariul <remondnicola@gmail.com>
2022-12-19 02:05:27 +0900

    🚨 Format code

--- src/problems/prob_31.rs
+++ src/problems/prob_31.rs
@@ -16,14 +16,12 @@ fn coin_ways(coins: &Vec<usize>, goal: usize) -> usize {
     n_ways
 }
 
-
 /// Compute the number of possible ways to make a specific amount (goal) with
 /// the UK coins : 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
 fn coin_ways_uk(goal: usize) -> usize {
     let coins = vec![1, 2, 5, 10, 20, 50, 100, 200];
     coin_ways(&coins, goal)
 }
-
 
 /// Solve the problem #31 and return the solution.
 pub fn solve() -> String {
