6a044afc63e4246005db0bfd32224ed6a9ca9571
Astariul <remondnicola@gmail.com>
2022-12-26 23:02:50 +0900

    ✨ Add solution to problem 36

---
+++ src/problems/prob_36.rs
@@ -0,0 +1,70 @@
+/// Check if a given number is a double palindrome
+fn is_double_base_palindrome(x: usize) -> bool {
+    // First, get the digits of the number in base 10
+    let mut digits = Vec::new();
+    let mut remain = x;
+    while remain > 0 {
+        digits.push(remain % 10);
+        remain = remain / 10;
+    }
+
+    // Check if it's a palindrome
+    for i in 0..digits.len() / 2 {
+        if digits[i] != digits[digits.len() - i - 1] {
+            return false;
+        }
+    }
+
+    // Then do the same for base 2
+    let mut digits = Vec::new();
+    let mut remain = x;
+    while remain > 0 {
+        digits.push(remain % 2);
+        remain = remain / 2;
+    }
+
+    for i in 0..digits.len() / 2 {
+        if digits[i] != digits[digits.len() - i - 1] {
+            return false;
+        }
+    }
+
+    true
+}
+
+/// Compute the sum of all double-base palindrome under a given limit.
+fn sum_double_base_palindromes(limit: usize) -> usize {
+    let mut sum = 0;
+
+    for n in 1..limit {
+        if is_double_base_palindrome(n) {
+            sum += n;
+        }
+    }
+    sum    
+}
+
+/// Solve the problem #36 and return the solution.
+pub fn solve() -> String {
+    sum_double_base_palindromes(1000000).to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        assert_eq!(sum_double_base_palindromes(15), 25);
+    }
+
+    #[test]
+    fn test_is_double_base_palindrome_true() {
+        assert!(is_double_base_palindrome(585));
+    }
+
+    #[test]
+    fn test_is_double_base_palindrome_false() {
+        assert!(!is_double_base_palindrome(19));
+    }
+}
