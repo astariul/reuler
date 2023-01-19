76b709ced0c075deb8ccbfc8b22260137eab571b
Astariul <remondnicola@gmail.com>
2023-01-20 00:51:12 +0900

    🐛 Fix problem 49

--- src/problems/prob_49.rs
+++ src/problems/prob_49.rs
@@ -10,23 +10,30 @@ fn prime_permutation() -> String {
         }
 
         // Find all possible permutations, but only keep the primes
-        let mut permutations: Vec<usize> = utils::permutations_of(utils::digits_of(p)).into_iter().map(|x| utils::digits_to_number(x)).filter(|x| utils::is_prime(*x)).collect();
+        let mut perm: Vec<usize> = utils::permutations_of(utils::digits_of(p)).into_iter().map(|x| utils::digits_to_number(x)).filter(|x| utils::is_prime(*x)).collect();
 
-        // Ignore the existing sequence
-        if permutations.contains(&1487) {
-            continue;
-        }
+        // Dedup the permutations (need sorting)
+        perm.sort();
+        perm.dedup();
 
-        // If we passed all the constraints, we found the other sequence !
-        if permutations.len() == 3 {
-            // Sort them and concatenate them
-            permutations.sort();
-
-            let mut result = String::new();
-            for n in permutations.iter() {
-                result.push_str(&n.to_string());
+        // If the size is right, check that the difference between each element
+        // is the same
+        if perm.len() >= 3 {
+            // We have to check all 3-subsequences 
+            for i in 0..perm.len() - 3 + 1 {
+                if perm[i + 1] - perm[i] == perm[i + 2] - perm[i + 1] {
+                    // We passed all the constraints ! Concatenate them and return
+                    let mut result = String::new();
+                    result.push_str(&perm[i].to_string());
+                    result.push_str(&perm[i + 1].to_string());
+                    result.push_str(&perm[i + 2].to_string());
+    
+                    // Only return if the result is not the existing sequence
+                    if result != "148748178147" {
+                        return result;
+                    }
+                }
             }
-            return result;
         }
     }
     panic!("Coudln't find the other sequence ?!")
