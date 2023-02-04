30c269e5eb7d73b0e455250aa4aa0f018594dd3a
Astariul <remondnicola@gmail.com>
2023-02-05 01:04:11 +0900

    ⚡️ Add a faster implementation

--- src/problems/prob_50.rs
+++ src/problems/prob_50.rs
@@ -2,31 +2,27 @@ use crate::utils;
 
 /// Find the prime that can be written as a sum of prime, under the given limit
 fn largest_consecutive_prime_sum_under(limit: usize) -> usize {
-    // Use dynamic programming to keep track of several sums
-    // sums[i][j] is the sum from prime index i to prime index i + j
-    let mut sums: Vec<Vec<usize>> = Vec::new();
-    for p in utils::Primes::new_up_to(limit) {
-        // Update all previous sums
-        for sum_i in sums.iter_mut() {
-            sum_i.push(sum_i[sum_i.len() - 1] + p);
-        }
+    let primes: Vec<usize> = utils::Primes::new_up_to(limit).collect();
 
-        // Add the last one
-        sums.push(vec![p]);
-    }
-
-    // Then, just find the sum that is prime and that has the biggest difference i-j
-    let mut best_prime = 2;
+    let mut best_sum = 0;
     let mut best_size = 0;
-    for sum_i in sums.iter() {
-        for (j, s) in sum_i.iter().enumerate() {
-            if j > best_size && s < &limit && utils::is_prime(*s) {
-                best_size = j;
-                best_prime = *s;
+
+    for i in 0..primes.len() {
+        let mut sum = 0;
+        for j in i..primes.len() {
+            sum += primes[j];
+
+            if sum > limit {
+                break;
+            }
+
+            if j - i > best_size && utils::is_prime(sum) {
+                best_sum = sum;
+                best_size = j - i;
             }
         }
     }
-    best_prime
+    best_sum
 }
 
 /// Solve the problem #50 and return the solution.
