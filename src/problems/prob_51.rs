ccaa058ab0a2ded13f47dd77a8c7a56ee5afa72e
Astariul <remondnicola@gmail.com>
2023-02-11 15:25:21 +0900

    ✨ Make problem 51 work

--- src/problems/prob_51.rs
+++ src/problems/prob_51.rs
@@ -1,5 +1,6 @@
 use crate::utils;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 // Compute all the possible arrangements of indexes in groups of n_index
 fn arrangements_of_index(indexes: Vec<usize>, n_index: usize) -> Vec<Vec<usize>> {
@@ -41,7 +42,7 @@ fn create_possible_replacements(x: usize, arrangements_cache: &mut HashMap<usize
                 for digit_to_replace in arr.iter() {
                     digits[*digit_to_replace] = d;
                 }
-                println!("->{}", utils::digits_to_number(digits.clone()));
+                // println!("->{}", utils::digits_to_number(digits.clone()));
                 curr_replacement.push(utils::digits_to_number(digits));
             }
             replacements.push(curr_replacement);
@@ -50,35 +51,113 @@ fn create_possible_replacements(x: usize, arrangements_cache: &mut HashMap<usize
     replacements
 }
 
+fn replace_ith_digit(digits: &Vec<usize>, i: usize) -> Vec<usize> {
+    let inversed_i = digits.len() - i - 1;
+    let mut family = Vec::new();
+    let start = if i == 0 { 1 } else { 0 };
+    for d in start..10 {
+        let mut member = digits.clone();
+        member[inversed_i] = d;
+        family.push(utils::digits_to_number(member));
+    }
+    family
+}
+
+fn replace_digit_i(digits: &Vec<usize>, i: usize) -> Vec<usize> {
+    let mut family = Vec::new();
+    let start = if digits[digits.len() - 1] == i { 1 } else { 0 };
+    for d in start..10 {
+        let member = digits.clone().into_iter().map(|m| if m == i { d } else { m }).collect();
+        family.push(utils::digits_to_number(member));
+    }
+    family
+}
+
+fn replace_repeated_digits(digits: &Vec<usize>) -> Vec<Vec<usize>> {
+    // First, find the repeated digits
+    let mut repeated_digits = Vec::new();
+    let mut seen_digits = HashSet::new();
+    for d in digits.iter() {
+        if seen_digits.contains(&d) {
+            repeated_digits.push(d);
+        } else {
+            seen_digits.insert(d);
+        }
+    }
+
+    let mut families = Vec::new();
+    for &digit_to_replace in repeated_digits.into_iter() {
+        families.push(replace_digit_i(digits, digit_to_replace));
+    }
+    families
+}
+
 /// Find the smallest prime where x digits can be replaced with the same digit,
 /// yielding n primes out of the 10 generated numbers.
 fn smallest_n_prime_replacement(n: usize) -> usize {
     if n > 10 {
         panic!("Can't generate {n} alternative numbers, there is only 10 digits.");
     }
+    if n < 1 {
+        panic!("Can't get the smallest number of a 0-family, it's empty. Specify a different n.");
+    }
 
-    let mut arrangements_cache = HashMap::new();
+    // let mut seen = HashSet::new();
     for p in utils::Primes::new() {
-        for replacement in create_possible_replacements(p, &mut arrangements_cache) {
-            println!("\nReplacements {p}:");
-            let mut n_primes = 0;
-            let mut min_r = usize::MAX;
-            for &r in replacement.iter() {
-                if utils::is_prime(r) {
-                    n_primes += 1;
-                    if r < min_r {
-                        min_r = r;
-                    }
-                }
-            }
+        // Skip already seen primes
+        // if seen.contains(&p) {
+        //     continue;
+        // }
 
-            if n_primes == n {
-                for &r in replacement.iter() {
-                    println!("{r}");
+        let p_digits = utils::digits_of(p);
+
+        // Replace single digit, only in case of 6-family or less
+        if n < 7 {
+            for i in 0..p_digits.len() {
+                let mut family = replace_ith_digit(&p_digits, i);
+                family.retain(|&m| utils::is_prime(m));
+                if family.len() == n {
+                    return *family.iter().min().unwrap();
                 }
-                return min_r;
             }
         }
+
+        // Replace repeated digits
+        let mut families = replace_repeated_digits(&p_digits);
+        for mut family in families.into_iter() {
+            family.retain(|&m| utils::is_prime(m));
+            if family.len() == n {
+                return *family.iter().min().unwrap();
+            }
+        }
+
+
+        // for replacement in create_possible_replacements(p, &mut arrangements_cache) {
+        //     let mut n_primes = 0;
+        //     let mut min_r = usize::MAX;
+        //     let mut s = false;
+        //     for &r in replacement.iter() {
+        //         if utils::is_prime(r) {
+        //             n_primes += 1;
+        //             if r < min_r {
+        //                 min_r = r;
+        //             }
+        //             if r == 56003 {
+        //                 println!("seen !");
+        //                 s = true;
+        //             }
+        //             seen.insert(r);
+        //         }
+        //     }
+
+        //     if s {
+        //         println!("==>{n_primes}");
+        //     }
+
+        //     if n_primes == n {
+        //         return min_r;
+        //     }
+        // }
     }
     panic!("Unreachable code");
 }
