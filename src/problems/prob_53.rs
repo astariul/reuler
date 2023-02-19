d54bc9ccfd889de3c82ac2d1969c311e33298e4e
Astariul <remondnicola@gmail.com>
2023-02-19 19:23:59 +0900

    🚧 New implementation, not compiling yet

--- src/problems/prob_53.rs
+++ src/problems/prob_53.rs
@@ -1,26 +1,37 @@
+use crate::utils;
 use std::collections::HashMap;
 
-fn cached_factorial(n: u128, cache: &mut HashMap<u128, u128>) -> u128 {
-    cache.entry(0).or_insert(1);
-    if !cache.contains_key(&n) {
-        let f = n * cached_factorial(n - 1, cache);
-        cache.insert(n, f);
-    }
-    cache[&n]
+struct CachedFactorial {
+    cache: HashMap<usize, utils::BigInt>,
 }
 
-/// Find the number of combinatorics selections where (n r) > above_x with
-/// any r, and 1 <= n <= max_n
-fn combinatorics_selections_above(max_n: u128, above_x: u128) -> usize {
-    let mut cache = HashMap::new();
+impl CachedFactorial {
+    fn new() -> Self {
+        let mut cache = HashMap::new();
+        cache.insert(0, utils::BigInt::from(1));
+        Self { cache }
+    }
+
+    fn of(&self, n: usize) -> utils::BigInt {
+        if !self.cache.contains_key(&n) {
+            let mut f = self.of(n - 1);
+            f *= n;
+            self.cache.insert(n, f);
+        }
+        self.cache[&n]
+    }
+}
+
+/// Find the number of combinatorics selections where (n r) has more than
+/// n_digits digits with any r, and 1 <= n <= max_n
+fn combinatorics_selections_above(max_n: usize, n_digits: usize) -> usize {
+    let factorial = CachedFactorial::new();
 
     let mut n_exceeding_value = 0;
     for n in 1..max_n + 1 {
         for r in 1..n + 1 {
-            println!("Yo wtf");
-            let x = cached_factorial(n, &mut cache) / (cached_factorial(r, &mut cache) * cached_factorial(n - r, &mut cache));
-            println!("{r} among {n} = {x}");
-            if x > above_x {
+            let x = factorial.of(n) / (factorial.of(r) * factorial.of(n - r));
+            if x.digits.len() > n_digits {
                 n_exceeding_value += 1;
             }
         }
@@ -30,7 +41,7 @@ fn combinatorics_selections_above(max_n: u128, above_x: u128) -> usize {
 
 /// Solve the problem #53 and return the solution.
 pub fn solve() -> String {
-    combinatorics_selections_above(100, 1000000).to_string()
+    combinatorics_selections_above(100, 6).to_string()
 }
 
 #[cfg(test)]
@@ -39,6 +50,6 @@ mod tests {
 
     #[test]
     fn test_given_example() {
-        assert_eq!(combinatorics_selections_above(23, 1000000), 4);
+        assert_eq!(combinatorics_selections_above(23, 6), 4);
     }
 }
