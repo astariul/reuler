8f3ba24e7ed5d44355485c10c4c292d8806d2784
Astariul <remondnicola@gmail.com>
2023-03-07 17:51:51 +0100

    ⚡️ Optimize solution of problem 57

--- src/problems/prob_57.rs
+++ src/problems/prob_57.rs
@@ -3,22 +3,35 @@ use crate::utils;
 /// When expanding the square root up to the given number of expansions, find
 /// the number of expansions that are "fat" : where the numerator has more
 /// digits than the denominator.
+///
+/// # How it works
+/// If we write the iterations recursively :
+/// f(n+1) = 1 + 1 / (1 + f(n))
+///
+/// If we write this as a fraction : f(n) = a(n) / b(n)
+///
+/// f(n+1) = 1 + 1 / (1 + a(n) / b(n))
+/// f(n+1) = 1 + 1 / ((b(n) + a(n)) / b(n))
+/// f(n+1) = 1 + b(n) / (b(n) + a(n))
+/// f(n+1) = (2 * b(n) + a(n)) / (b(n) + a(n))
+///
+/// So we have :
+/// a(n+1) = 2 * b(n) + a(n)
+/// b(n+1) = a(n) + b(n)
 fn n_fat_square_root_expansions(n_expansions: usize) -> usize {
     let mut n_fat_expansions = 0;
-    let mut curr_expansion = 1.0;
-    let mut expansion_idx = 0;
+    let mut a = utils::BigInt::from(1);
+    let mut b = utils::BigInt::from(1);
+    let mut expansion_i = 0;
 
-    let mut denominator = 2;
-    while expansion_idx < n_expansions {
-        curr_expansion = 1.0 + (1.0 / (1.0 + curr_expansion));
-        expansion_idx += 1;
+    while expansion_i < n_expansions {
+        let new_b = &a + &b;
+        b *= 2;
+        a = &b + &a;
+        b = new_b;
+        expansion_i += 1;
 
-        while (denominator as f64 * curr_expansion).fract() > 0.0 {
-            denominator += 1;
-        }
-        let numerator = (denominator as f64 * curr_expansion) as usize;
-
-        if utils::BigInt::from(numerator).digits.len() > utils::BigInt::from(denominator).digits.len() {
+        if a.digits.len() > b.digits.len() {
             n_fat_expansions += 1;
         }
     }
